765617711109e33ac9d40bdfbeb6a21a001c8046
azahinhasan <azahinhasan@gmail.com>
2024-07-28 22:08:06 +0600

    refactor: validation add into findAge

--- functions/find_dob.js
+++ functions/find_dob.js
@@ -2,11 +2,19 @@ const dobIs = (age, startDate) => {
   //stateDate should be MM/DD/YYYY
   if (startDate && new Date(startDate).toString() == "Invalid Date")
     return "Invalid Start Date";
+  if (
+    !age ||
+    (age.year && typeof age.year !== "number") ||
+    (age.month && typeof age.month !== "number") ||
+    (age.day && typeof age.day !== "number")
+  )
+    return "Invalid Age Date";
   else startDate = startDate ? new Date(startDate) : new Date();
+
   const birthDate = new Date(
-    startDate.getFullYear() - age.year,
-    startDate.getMonth() - age.month,
-    startDate.getDate() - age.day
+    startDate.getFullYear() - (age.year ?? 0),
+    startDate.getMonth() - (age.month ?? 0),
+    startDate.getDate() - (age.day ?? 0)
   );
   birthDate.setDate(birthDate.getDate() + 1);
   return birthDate;
