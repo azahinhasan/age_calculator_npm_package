8ce6e9ce5f8101fc73def64bf174ac120735f1cf
azahinhasan <azahinhasan@gmail.com>
2024-07-30 23:15:51 +0600

    working on optimization

--- functions/find_age.js
+++ functions/find_age.js
@@ -1,11 +1,14 @@
-const ageIs = (date, startDate) => {
-  if (!date || new Date(date).toString() == "Invalid Date")
+const ageIs = (dob, startDate) => {
+  if (!dob || new Date(dob).toString() == "Invalid Date")
     return "Invalid DOB Date";
   if (startDate && new Date(startDate).toString() == "Invalid Date")
     return "Invalid Start Date Date";
 
-  const [month, day, year] = date.split("/").map(Number);
-  const birthDate = new Date(year, month - 1, day);
+  const [dobMonth, dobDay, dobYear] = dob.split("/").map(Number);
+
+  const birthDate = new Date(dob);
+  console.log();
+
   const today = startDate ? new Date(startDate) : new Date();
 
   let ageYears = today.getFullYear() - birthDate.getFullYear();
@@ -14,22 +17,22 @@ const ageIs = (date, startDate) => {
 
   if (ageDays < 0) {
     ageMonths--;
-    const lastMonth = new Date(year, month, 0);
+    const lastMonth = new Date(dobYear, dobMonth, 0);
     ageDays += lastMonth.getDate();
   }
 
   if (
-    month === 2 &&
-    ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) &&
-    day > today.getDate()
+    dobMonth === 2 &&
+    ((dobYear % 4 === 0 && dobYear % 100 !== 0) || dobYear % 400 === 0) &&
+    dobDay > today.getDate()
   ) {
     // console.log(
-    //   month,
-    //   date,
-    //   day,
+    //   dobMonth,
+    //   dob,
+    //   dobDay,
     //   today.getDate(),
     //   today.getMonth(),
-    //   (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+    //   (dobYear % 4 === 0 && dobYear % 100 !== 0) || dobYear % 400 === 0
     // );
     ageDays += today.getMonth() < 4 ? 2 : 1;
   }
